87dc30d35b3a61946a83009db87e56ac17fd6d9e
Jordane-Kraken <jordane.minaya@gmail.com>
2021-03-09 16:40:40 +0100

    modif chemin des images et dossiers music et images déplacés dans public

--- music-player-react/src/components/App/App.js
+++ music-player-react/src/components/App/App.js
@@ -4,34 +4,52 @@ import Player from '../Player';
 function App() {
     const [songs, setSongs] = useState([
       {
+        title : "Every night",
+        artist: "Zmeyev",
+        img_src: "./images/song-1.jpg",
+        src: "./music/zmeyev-every-night.mp3"
+      },
+      {
+        title : "Chamomille",
+        artist: "Austin Marc",
+        img_src: "./images/song-2.jpg",
+        src: "./music/chamomille.mp3"
+      },
+      {
+        title : "Pink sky",
+        artist: "Sky Pedro",
+        img_src: "./images/song-3.jpg",
+        src: "./music/pink-sky.mp3"
+      },
+      {
         title : "Obidos",
         artist: "Glimlip, Sling Dilly",
-        img_src: "../../assets/images/song-4.jpeg",
-        src: "../../assets/music/glimlip-x-sling-dilly-obidos.mp3"
+        img_src: "./images/song-4.jpeg",
+        src: "./music/glimlip-x-sling-dilly-obidos.mp3"
         },
         {
         title : "When There's Nothing Else",
         artist: "LeVirya, Broey",
-        img_src: "../../assets/images/song-5.jpg",
-        src: "../../assets/music/when-theres-nothing-else.mp3"
+        img_src: "./images/song-5.jpg",
+        src: "./music/when-theres-nothing-else.mp3"
         },
         {
         title : "Sunset",
         artist: "Odyssee, Gatz2Gatz",
-        img_src: "../../assets/images/song-6.jpg",
-        src: "../../assets/music/odyssee-gatz2gatz-sunset.mp3"
+        img_src: "./assets/images/song-6.jpg",
+        src: "./assets/music/odyssee-gatz2gatz-sunset.mp3"
         },
         {
         title : "Ylang Ylang",
         artist: "FKJ",
-        img_src: "../../assets/images/song-7.jpg",
-        src: "../../assets/music/fkj-ylang-ylang.mp3"
+        img_src: "./images/song-7.jpg",
+        src: "./music/fkj-ylang-ylang.mp3"
         },
         {
         title : "Soulful",
         artist: "L'indécis",
-        img_src: "../assets/images/song-8.jpg",
-        src: "../../assets/music/lindecis-soulful.mp3"
+        img_src: "./images/song-8.jpg",
+        src: "./music/lindecis-soulful.mp3"
         },
     ]);
 
--- music-player-react/src/components/Player/index.js
+++ music-player-react/src/components/Player/index.js
@@ -1,10 +1,12 @@
 import { React } from 'react';
+import PlayerDetails from './PlayerDetails';
 
 function Player(props) {
   return (
     <div className="player">
       <audio></audio>
       <h4>Playing Now</h4>
+      <PlayerDetails song={props.song} />
       <p>Next : {props.nextSong.title} by {props.nextSong.artist}</p>
     </div>
   )
